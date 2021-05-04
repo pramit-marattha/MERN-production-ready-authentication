1e0fa3ab9c38e6f0187233ae245ac7e0ed8b5859
pramit-marattha <pramitmarattha5497@gmail.com>
2021-05-04 23:53:04 +0545

    private routing feature implemented

--- client/src/components/routing/Private.js
+++ client/src/components/routing/Private.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Redirect, Route } from "react-router-dom";
 
-const Private = () => {
-  return <div></div>;
+const Private = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("authToken") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
 };
 
 export default Private;
