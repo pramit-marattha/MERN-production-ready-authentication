93b15c770d0ffe91164a823913a367bb97c9e0a8
pramit-marattha <pramitmarattha5497@gmail.com>
2021-05-04 23:52:29 +0545

    route path added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import react from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/forgot-password" component={ForgotPassword} />
+        <Route
+          exact
+          path="/password-reset/:resetToken"
+          component={ResetPassword}
+        />
+      </Switch>
       <div className="App">Client</div>
-    </>
+    </BrowserRouter>
   );
 }
 
