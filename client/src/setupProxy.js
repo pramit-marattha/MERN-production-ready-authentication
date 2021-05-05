8ae7d31879652752e2ee0c351545958a73dc6e66
pramit-marattha <pramitmarattha5497@gmail.com>
2021-05-05 17:00:28 +0545

    proxy route fixed

---
+++ client/src/setupProxy.js
@@ -0,0 +1,12 @@
+// import createProxyMiddleware from "http-proxy-middleware";
+const createProxyMiddleware = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: process.env.REACT_APP_PROXY_ROUTE,
+      changeOrigin: true,
+    })
+  );
+};
